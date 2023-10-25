544d99cc30f260c707f4c444b3695af2cf04a1e6
Arindam Chakraborty <arindamckbt@gmail.com>
2023-10-26 02:20:03 +0530

    Flat Member due and present payment linkage

--- js/flat_member_landingPage.js
+++ js/flat_member_landingPage.js
@@ -1,28 +1,17 @@
 document.addEventListener("DOMContentLoaded", async function () {
+    const sessionTimeout = 600000;
     // Retrieve the username from the URL parameter
     const urlParams = new URLSearchParams(window.location.search);
     const username = urlParams.get("username");
 
 
-// const userData = {
-//     ownerName: "",
-//     flatNumber: "D5",
-//     blockNumber: "1",
-//     contactDetails:username,
-// };
-// userData.contactDetails = username;
 
-// const paymentHistory = [
-//     { date: "", id:"",updues: "",penddues:"", receipt: "" },
-//     ,
-// ];
 
 const paymentHistory = [];
+// const paymentAmount=[];
 
-const paymentAmount = {
-    udues: "Rs.100.00", 
-    pdues: "Rs.200.00",
-};
+checkSessionTimeout(sessionTimeout);
+setInterval(() => checkSessionTimeout(sessionTimeout), 1000);
 
 
 
@@ -32,6 +21,28 @@ const userData = {
     blockNumber: "",
     contactDetails: username, // Set contactDetails from the URL parameter
 };
+
+
+const paymentAmount = {
+    udues: "", 
+    pdues: "",
+};
+
+// Function to check and log out if the session has expired
+function checkSessionTimeout() {
+    const sessionStartTime = sessionStorage.getItem("sessionStartTime");
+    if (sessionStartTime) {
+        const currentTime = new Date().getTime();
+        if (currentTime - parseInt(sessionStartTime, 10) > sessionTimeout) {
+            // Session has expired, clear session data
+            sessionStorage.clear();
+            window.location.href = "index.html"; // Redirect to the login page
+        }
+    }
+}
+
+
+
 
 // Function to make the GET request and update ownerName
 async function fetchOwnerName() {
@@ -143,151 +154,7 @@ async function fetchPaymentTable() {
     }
 }
 
-
-
-
-
-// async function fetchPaymentTable() {
-//     try {
-//         const response = await fetch(`http://localhost:8080/payment/userPaymentList/${userData.contactDetails}`);
-//         if (response.ok) {
-//             const data = await response.json();
-//             console.log("API Response:", data);
-//             // Assuming `data` is an array, you can push it into the paymentHistory array
-//             // paymentHistory.date=data.createdAt
-//             // paymentHistory.id=data.id
-//             // paymentHistory.penddues=data.dues
-//             // paymentHistory.udues=data.paymentAmount
-//             // paymentHistory.receipt=data.receipt
-//             // paymentHistory.push({
-//             //     id: data.id,
-//             //     date: data.createdAt,
-//             //     updues: data.paymentAmount,
-//             //     penddues: data.dues,
-//             //     receipt: data.receipt
-//             // });
-//             // populatePaymentHistory();
-//             // const paymentTable = document.getElementById("paymentTable");
-//             //     const tbody = paymentTable.querySelector("tbody");
-
-//             //     tbody.innerHTML = ''; // Clear the existing table rows
-//             //     paymentHistory.forEach((payment) => {
-//             //         const row = document.createElement("tr");
-//             //         row.innerHTML = `
-//             //             <td>${payment.id}</td>
-//             //             <td>${payment.date}</td>
-//             //             <td>${payment.updues}</td>
-//             //             <td>${payment.penddues}</td>
-//             //             <td><a href="${payment.receipt}" target="_blank">View Receipt</a></td>
-//             //         `;
-//             //         tbody.appendChild(row);
-//             //     });
-//             data.forEach((paymentData) => {
-//                 // Create a new payment object and push it to the paymentHistory array
-//                 const payment = {
-//                     id: paymentData.id,
-//                     date: paymentData.createdAt,
-//                     updues: paymentData.paymentAmount,
-//                     penddues: paymentData.dues,
-//                     receipt: paymentData.receipt
-//                 };
-//                 paymentHistory.push(payment);
-//                 const viewReceiptLink = document.createElement("a");
-//                 viewReceiptLink.href = payment.receipt;
-//                 viewReceiptLink.textContent = "View Receipt";
-//                 viewReceiptLink.target = "_blank";
-//                 viewReceiptLink.classList.add("view-receipt");
-//                 viewReceiptLink.setAttribute("data-payment-id", paymentData.id);
-
-//                 // Now, you can handle the click event for the "View Receipt" link
-//                 viewReceiptLink.addEventListener("click", async (event) => {
-//                     event.preventDefault(); // Prevent the default link behavior
-
-//                     const paymentId = viewReceiptLink.getAttribute("data-payment-id");
-//                     if (paymentId) {
-//                         // Make an API request to generate the PDF
-//                         try {
-//                             const response = await fetch(`http://localhost:8080/invoice/generate/${paymentId}`);
-//                             if (response.ok) {
-//                                 // Successfully generated the PDF, you can do something with it
-//                                 const pdfData = await response.blob();
-//                                 const pdfUrl = window.URL.createObjectURL(pdfData);
-//                                 // For example, open the PDF in a new tab
-//                                 window.open(pdfUrl);
-//                             } else {
-//                                 console.error("Error generating PDF receipt.");
-//                             }
-//                         } catch (error) {
-//                             console.error("An error occurred:", error);
-//                         }
-//                     }
-//                 });
-//             });
-
-//             // Now that paymentHistory is populated, you can call populatePaymentHistory
-//             populatePaymentHistory();
-//         } else {
-//             console.error("Error fetching data.");
-//         }
-//     } catch (error) {
-//         console.error("An error occurred:", error);
-//     }
-// }
-
-// function populatePaymentHistory() {
-//     const paymentTable = document.getElementById("paymentTable");
-//     const tbody = paymentTable.querySelector("tbody");
-
-//     paymentHistory.forEach((payment) => {
-//         const row = document.createElement("tr");
-//         row.innerHTML = `
-//             <td>${payment.id}</td>
-//             <td>${payment.date}</td>
-//             <td>${payment.updues}</td>
-//             <td>${payment.penddues}</td>
-//             <td></td>
-//         `;
-//                     const lastCell = row.querySelector("td:last-child");
-//                     // lastCell.appendChild(viewReceiptLink);
-//         tbody.appendChild(row);
-//     });
-// }
-// populatePaymentHistory();
 await fetchPaymentTable();
-
-
-// const viewReceiptLinks = document.querySelectorAll(".view-receipt");
-// viewReceiptLinks.forEach(link => {
-//     link.addEventListener("click", async (event) => {
-//         event.preventDefault(); // Prevent the default link behavior
-
-//         const paymentId = link.getAttribute("data-payment-id");
-//         if (paymentId) {
-//             // Make a POST API request to generate the PDF
-//             try {
-//                 const response = await fetch(`http://localhost:8080/invoice/generate/${paymentId}`, {
-//                     method: "POST",
-//                     headers: {
-//                         "Content-Type": "application/json",
-//                     },
-//                     body: JSON.stringify({ paymentId: paymentId }),
-//                 });
-
-//                 if (response.ok) {
-//                     // Successfully generated the PDF, you can do something with it
-//                     const pdfData = await response.blob();
-//                     const pdfUrl = window.URL.createObjectURL(pdfData);
-//                     // For example, open the PDF in a new tab
-//                     window.open(pdfUrl);
-//                 } else {
-//                     console.error("Error generating PDF receipt.");
-//                 }
-//             } catch (error) {
-//                 console.error("An error occurred:", error);
-//             }
-//         }
-//     });
-// });
 
 const viewReceiptLinks = document.querySelectorAll(".view-receipt");
 viewReceiptLinks.forEach(link => {
@@ -339,45 +206,109 @@ viewReceiptLinks.forEach(link => {
 
 
 
+async function fetchamountanddues() {
+    try {
+        const response = await fetch(`http://localhost:8080/flat-details/${userData.contactDetails}`);
+        if (response.ok) {
+            const data = await response.json();
+            console.log("API Response:", data);
+            paymentAmount.udues = data.amount;
+            paymentAmount.pdues = data.dueAmount;
+            console.log("paymentAmount.udues:", paymentAmount.udues);
+            console.log("paymentAmount.pdues:", paymentAmount.pdues);
+            populatePaymentAmount();
+        }
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+}
+
 
 function populatePaymentAmount() {
     document.getElementById("uDues").textContent = paymentAmount.udues;
     document.getElementById("pDues").textContent = paymentAmount.pdues;
 }
+await fetchamountanddues();
+
+
 
 
 document.getElementById("generateReceiptBtn").addEventListener("click", () => {
     alert("PDF generation functionality will be added here.");
 });
-document.getElementById("makeUPayment").addEventListener("click", () => {
-    alert("You will be redirected to the payment page");
-});
-document.getElementById("makePPayment").addEventListener("click", () => {
-    alert("You will be redirected to the payment page");
-});
 
-populateUserData();
-// populatePaymentHistory();
-populatePaymentAmount();
+async function makeUPayment() {
+    // You can perform the payment process here
+    const amountToPay = paymentAmount.udues;
+
+    // Assuming you have a payment gateway or some API to process the payment
+    // You can make an API request to initiate the payment process
+    try {
+        const response = await fetch(`http://localhost:8080/flat-details`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ amount: amountToPay }),
+        });
+
+        if (response.ok) {
+            // Payment was successful, you can handle the success scenario
+            alert(`Payment of $${amountToPay} was successful.`);
+        } else {
+            // Handle payment failure
+            alert("Payment failed. Please try again later.");
+        }
+    } catch (error) {
+        console.error("An error occurred:", error);
+        // Handle payment error
+        alert("An error occurred during the payment process.");
+    }
+}
+
+document.getElementById("makeUPayment").addEventListener("click", makeUPayment);
+// document.getElementById("makeUPayment").addEventListener("click", () => {
+//     alert("You will be redirected to the payment page");
+// });
+async function makePPayment() {
+    // You can perform the payment process here
+    const amountToPay = paymentAmount.pdues;
+
+    // Assuming you have a payment gateway or some API to process the payment
+    // You can make an API request to initiate the payment process
+    try {
+        const response = await fetch(`http://localhost:8080/flat-details`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ amount: amountToPay }),
+        });
+
+        if (response.ok) {
+            // Payment was successful, you can handle the success scenario
+            alert(`Payment of $${amountToPay} was successful.`);
+        } else {
+            // Handle payment failure
+            alert("Payment failed. Please try again later.");
+        }
+    } catch (error) {
+        console.error("An error occurred:", error);
+        // Handle payment error
+        alert("An error occurred during the payment process.");
+    }
+}
+
+document.getElementById("makePPayment").addEventListener("click", makePPayment);
 
 
 function logout() {
-    localStorage.clear();
     sessionStorage.clear();
     window.location.href = "index.html";
 }
-// function clearSession() {
-//     sessionStorage.clear(); 
-// }
-window.onpopstate = function(event) {
-    if (event.state && event.state.clearSession) {
-        clearSession();
-    }
-};
-function clearSessionOnBackOrForward() {
-    history.pushState({ clearSession: true }, null, null);
-}
-sessionStorage.setItem('key', 'value');
 document.getElementById("logoutButton").addEventListener("click", logout);
+
+
+
 
 });
