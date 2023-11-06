970e9c9452b76540eb33572c4a7452fcae79ef83
Arindam Chakraborty <arindamckbt@gmail.com>
2023-11-07 02:26:20 +0530

    Financial features and login timing update

--- js/cm_account.js
+++ js/cm_account.js
@@ -1,29 +1,38 @@
+document.querySelector(".home").addEventListener("click", function () {
+    window.location.href = "committeepage.html";
+});
 
+function showCollection() {
+    const datePicker = document.getElementById("datePicker").value;
+    const paymentTableBody = document.getElementById("paymentTableBody");
+    paymentTableBody.innerHTML = ""; // Clear the table body
 
-const paymentHistory = [
-    { date: "2023-09-01", bank: "Rs 500.00", online: "Rs 501.00", offline:"Rs 502.00", expense: "Rs 2.00", balance: "Rs 100.00", deficit: "Rs 1" },
-];
-function populatePaymentHistory() {
-    const paymentTable = document.getElementById("paymentTable");
-    const tbody = paymentTable.querySelector("tbody");
+    // You need to fetch your data (e.g., from the "cm_account.js" script) and filter by date here.
+    // For demonstration, I'll create a sample data array.
 
-    paymentHistory.forEach((payment) => {
-        const row = document.createElement("tr");
-        row.innerHTML = `
-            <td>${payment.date}</td>
-            <td>${payment.bank}</td>
-            <td>${payment.online}</td>
-            <td>${payment.offline}</td>
-            <td>${payment.expense}</td>
-            <td>${payment.balance}</td>
-            <td>${payment.deficit}</td>
-        `;
-        tbody.appendChild(row);
+    const sampleData = [
+        { date: "2023-11-07", bankSavings: 1000, onlineCollection: 500, offlineCollection: 300, expense: 200, balance: 400, deficit: 0 },
+        { date: "2023-11-08", bankSavings: 1200, onlineCollection: 600, offlineCollection: 400, expense: 250, balance: 550, deficit: 0 },
+        // Add more data as needed
+    ];
+
+    sampleData.forEach(item => {
+        if (item.date === datePicker) {
+            const row = paymentTableBody.insertRow();
+            const dateCell = row.insertCell(0);
+            const bankSavingsCell = row.insertCell(1);
+            const onlineCollectionCell = row.insertCell(2);
+            const offlineCollectionCell = row.insertCell(3);
+            const expenseCell = row.insertCell(4);
+            const balanceCell = row.insertCell(5);
+            const deficitCell = row.insertCell(6);
+            dateCell.textContent = item.date;
+            bankSavingsCell.textContent = item.bankSavings;
+            onlineCollectionCell.textContent = item.onlineCollection;
+            offlineCollectionCell.textContent = item.offlineCollection;
+            expenseCell.textContent = item.expense;
+            balanceCell.textContent = item.balance;
+            deficitCell.textContent = item.deficit;
+        }
     });
 }
-
-populatePaymentHistory();
-populatePaymentAmount();
-
-
-
